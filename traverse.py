7501b3531739fdeb2dadba7dbc40402d460252fd
Diana Badas <diana.badas@gmail.com>
2020-02-19 11:23:32 +0000

    Traverse tree

---
+++ traverse.py
@@ -0,0 +1,6 @@
+"""
+We can do it 
+    in order : left subtree + root node + right subtree recursively. It sorts the tree.
+    pre order : root + left subtree + right subtree recursively.
+    post order : left + right + root recursively
+"""
